73f1afa6810531afe5aa45ad72c87908ef6d5606
Christian Hitz <christian@klarinett.li>
2016-02-13 17:34:35 +0100

    refactor code: add Connection class

--- main.cpp
+++ main.cpp
@@ -13,79 +13,14 @@ using asio::ip::tcp;
 
 typedef std::deque<std::string> message_queue;
 
-class ButtonEventListener: public flic::client::button::ButtonEventListener {
+class Connection {
 public:
-	ButtonEventListener(asio::io_service& io_service, tcp::socket& socket,
-			const std::string& deviceId) :
-			io_service_(io_service), socket_(socket), id_(deviceId) {
-		std::cout << deviceId << " collected" << std::endl;
-
-		dsuid_generate_v3_from_namespace(DSUID_NS_IEEE_MAC, id_.c_str(),
-				&dsuid_);
-
-		write(fmt::format("{{'message':'init',"
-				" 'protocol':'json',"
-				" 'tag':'{}',"
-				" 'uniqueid':'{}',"
-				" 'group': 8,"
-				" 'hardwarename':'Flic button',"
-				" 'modelname':'Flic button',"
-				" 'name':'Flic',"
-				" 'buttons':[{{'id':0,'buttontype':1,'group':1}}],"
-				" 'inputs': [{{'inputtype':12,'usage':1}},{{'inputtype':1,'usage':1}}]"
-				"}}\n", id_, strdsuid(&dsuid_)));
-		onConnect(deviceId);
-		onBatteryStatus(deviceId, 100);
+	Connection(asio::io_service& io_service,
+			tcp::resolver::iterator endpoint_iterator) :
+			io_service_(io_service), socket_(io_service) {
+		do_connect(endpoint_iterator);
 	}
 
-	virtual std::string getHash() override {
-		return "main";
-	}
-
-	virtual void onButtonUpOrDown(const std::string& deviceId,
-			const bool& queued, const int& timeDiff, const bool& isUp,
-			const bool& isDown) override {
-		//ignore any clicks that are older than 15 seconds
-		if (queued && (timeDiff > 15)) {
-			return;
-		}
-		std::cout << deviceId << (isUp ? " up" : " down") << std::endl;
-		write(fmt::format("{{'message':'button',"
-				" 'index':0,"
-				" 'value':{},"
-				" 'tag':'{}'"
-				"}}\n", (isUp ? 0 : 1), id_));
-	}
-
-	virtual void onConnect(const std::string& deviceId) override {
-		std::cout << deviceId << " ready" << std::endl;
-		write(fmt::format("{{'message':'input',"
-				" 'index':1,"
-				" 'value':1,"
-				" 'tag':'{}'"
-				"}}\n", id_));
-	}
-
-	virtual void onDisconnect(const std::string& deviceId) override {
-		std::cout << deviceId << " disconnected" << std::endl;
-		write(fmt::format("{{'message':'input',"
-				" 'index':1,"
-				" 'value':0,"
-				" 'tag':'{}'"
-				"}}\n", id_));
-	}
-
-	virtual void onBatteryStatus(const std::string& deviceId,
-			const int& battery) override {
-		std::cout << deviceId << " battery is: " << battery << std::endl;
-		write(fmt::format("{{'message':'input',"
-				" 'index':0,"
-				" 'value':{},"
-				" 'tag':'{}'"
-				"}}\n", (battery < 10 ? 1 : 0), id_));
-	}
-
-private:
 	void write(const std::string& msg) {
 		std::cout << "write: " << msg << std::endl;
 		io_service_.post([this, msg]()
@@ -98,60 +33,6 @@ private:
 			}
 		});
 	}
-
-	void do_write() {
-		asio::async_write(socket_,
-				asio::buffer(write_msgs_.front().data(),
-						write_msgs_.front().length()),
-				[this](std::error_code ec, std::size_t /*length*/)
-				{
-					if (!ec)
-					{
-						write_msgs_.pop_front();
-						if (!write_msgs_.empty())
-						{
-							do_write();
-						}
-					}
-					else
-					{
-						std::cout << "write failed with: " << ec << std::endl;
-						socket_.close();
-					}
-				});
-	}
-
-private:
-	asio::io_service& io_service_;
-	tcp::socket& socket_;
-	message_queue write_msgs_;
-	std::string id_;
-	dsuid_t dsuid_;
-};
-
-class ButtonListener: public flic::client::manager::ButtonListener {
-private:
-	std::shared_ptr<flic::client::manager::Manager> manager;
-public:
-	ButtonListener(std::shared_ptr<flic::client::manager::Manager> manager,
-			asio::io_service& io_service, tcp::socket& socket,
-			tcp::resolver::iterator endpoint_iterator) :
-			manager(manager), io_service_(io_service), socket_(socket) {
-		do_connect(endpoint_iterator);
-	}
-
-	virtual std::string getHash() override {
-		return "main";
-	}
-
-	virtual void onButtonDiscover(const std::string& deviceId) override {
-		auto button = manager->getButton(deviceId);
-		button->addButtonEventListener(
-				std::shared_ptr<flic::client::button::ButtonEventListener>(
-						new ButtonEventListener(io_service_, socket_, deviceId)));
-	}
-
-	tcp::socket& getSocket() const { return socket_; }
 
 private:
 	void do_connect(tcp::resolver::iterator endpoint_iterator) {
@@ -184,10 +65,135 @@ private:
 				});
 	}
 
+	void do_write() {
+		asio::async_write(socket_,
+				asio::buffer(write_msgs_.front().data(),
+						write_msgs_.front().length()),
+				[this](std::error_code ec, std::size_t /*length*/)
+				{
+					if (!ec)
+					{
+						write_msgs_.pop_front();
+						if (!write_msgs_.empty())
+						{
+							do_write();
+						}
+					}
+					else
+					{
+						std::cout << "write failed with: " << ec << std::endl;
+						socket_.close();
+					}
+				});
+	}
 private:
 	asio::io_service& io_service_;
-	tcp::socket& socket_;
+	tcp::socket socket_;
 	char read_msg_[128];
+	message_queue write_msgs_;
+};
+
+class ButtonEventListener: public flic::client::button::ButtonEventListener {
+public:
+	ButtonEventListener(Connection &connection, const std::string& deviceId) :
+			connection_(connection), id_(deviceId) {
+		std::cout << deviceId << " collected" << std::endl;
+
+		dsuid_generate_v3_from_namespace(DSUID_NS_IEEE_MAC, id_.c_str(),
+				&dsuid_);
+
+		connection_.write(
+				fmt::format(
+						"{{'message':'init',"
+								" 'protocol':'json',"
+								" 'tag':'{}',"
+								" 'uniqueid':'{}',"
+								" 'group': 8,"
+								" 'hardwarename':'Flic button',"
+								" 'modelname':'Flic button',"
+								" 'name':'Flic',"
+								" 'buttons':[{{'id':0,'buttontype':1,'group':1}}],"
+								" 'inputs': [{{'inputtype':12,'usage':1}},{{'inputtype':1,'usage':1}}]"
+								"}}\n", id_, strdsuid(&dsuid_)));
+		onConnect(deviceId);
+		onBatteryStatus(deviceId, 100);
+	}
+
+	virtual std::string getHash() override {
+		return "main";
+	}
+
+	virtual void onButtonUpOrDown(const std::string& deviceId,
+			const bool& queued, const int& timeDiff, const bool& isUp,
+			const bool& isDown) override {
+		//ignore any clicks that are older than 15 seconds
+		if (queued && (timeDiff > 15)) {
+			return;
+		}
+		std::cout << deviceId << (isUp ? " up" : " down") << std::endl;
+		connection_.write(fmt::format("{{'message':'button',"
+				" 'index':0,"
+				" 'value':{},"
+				" 'tag':'{}'"
+				"}}\n", (isUp ? 0 : 1), id_));
+	}
+
+	virtual void onConnect(const std::string& deviceId) override {
+		std::cout << deviceId << " ready" << std::endl;
+		connection_.write(fmt::format("{{'message':'input',"
+				" 'index':1,"
+				" 'value':1,"
+				" 'tag':'{}'"
+				"}}\n", id_));
+	}
+
+	virtual void onDisconnect(const std::string& deviceId) override {
+		std::cout << deviceId << " disconnected" << std::endl;
+		connection_.write(fmt::format("{{'message':'input',"
+				" 'index':1,"
+				" 'value':0,"
+				" 'tag':'{}'"
+				"}}\n", id_));
+	}
+
+	virtual void onBatteryStatus(const std::string& deviceId,
+			const int& battery) override {
+		std::cout << deviceId << " battery is: " << battery << std::endl;
+		connection_.write(fmt::format("{{'message':'input',"
+				" 'index':0,"
+				" 'value':{},"
+				" 'tag':'{}'"
+				"}}\n", (battery < 10 ? 1 : 0), id_));
+	}
+
+private:
+	Connection& connection_;
+	std::string id_;
+	dsuid_t dsuid_;
+};
+
+class ButtonListener: public flic::client::manager::ButtonListener {
+private:
+	std::shared_ptr<flic::client::manager::Manager> manager;
+public:
+	ButtonListener(Connection &connection,
+			std::shared_ptr<flic::client::manager::Manager> manager) :
+			connection_(connection), manager(manager) {
+	}
+
+	virtual std::string getHash() override {
+		return "main";
+	}
+
+	virtual void onButtonDiscover(const std::string& deviceId) override {
+		auto button = manager->getButton(deviceId);
+		button->addButtonEventListener(
+				std::shared_ptr<flic::client::button::ButtonEventListener>(
+						new ButtonEventListener(connection_, deviceId)));
+	}
+
+private:
+	Connection& connection_;
 };
 
 int main() {
@@ -204,24 +210,25 @@ int main() {
 			std::cout << endpoint.endpoint() << std::endl;
 		}
 
-		std::thread t;
-		tcp::socket socket(io_service);
+		Connection connection(io_service, endpoint_iterator);
+		std::thread t([&io_service]() {
+			std::cout << "ASIO thread started" << std::endl;
+			io_service.run();
+			std::cout << "ASIO thread exited" << std::endl;
+		});
 
 		client.start(
-				[&client, &io_service, &endpoint_iterator, &t, &socket] () {
+				[&client, &connection] () {
 					std::cout << "Initialized" << std::endl;
 					auto manager = client.getManager();
 
 					manager->addButtonListener(
 							std::shared_ptr<flic::client::manager::ButtonListener>(
-									new ButtonListener(manager, io_service, socket,
-											endpoint_iterator)));
-
-					t = std::thread([&io_service]() {std::cout << "ASIO thread started" << std::endl; io_service.run(); std::cout << "ASIO thread exited" << std::endl;});
+									new ButtonListener(connection, manager)));
 
 					for (auto& button : manager->getButtons()) {
 						button->addButtonEventListener(std::shared_ptr<flic::client::button::ButtonEventListener>(
-								new ButtonEventListener(io_service, socket, button->getDeviceId())));
+										new ButtonEventListener(connection, button->getDeviceId())));
 					}
 				}, [&client] (const bool& resumable) {
 					std::cout << "Uninitialized" << std::endl;
